f98ea0aeae59a57ab7df1bcfe4511367969b6c68
Fateh Ali Khan <fatehalikhanh@gmail.com>
2019-07-14 19:12:01 +0530

    More Fixes

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import './scss/index.scss'
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+
+serviceWorker.register();
